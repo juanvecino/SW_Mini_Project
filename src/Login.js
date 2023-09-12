c3ace9ff0ccea4b9ba5fd72af5dd5aa82374e9d8
juanvecino <49287699+juanvecino@users.noreply.github.com>
2023-09-11 22:19:20 -0400

    Adding Google Login

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,39 @@
 import React, { useState } from 'react';
+import GoogleButton from 'react-google-button';
+import {auth, provider} from './components/firebase';
+import { signInWithPopup,GoogleAuthProvider} from "firebase/auth";
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+
+  function signUp() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+
+  }
+
+  function signIn() {
+    
+  }
 
   const handleLogin = () => {
     // Implement your authentication logic here
@@ -35,6 +66,7 @@ const Login = () => {
       <button className="login-button" onClick={handleLogin}>
         Login
       </button>
+      <GoogleButton onClick={signUp}/>
     </div>
   );
 };
---
+++ src/components/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBprfAnRPmRD2z69XZTu5gLVl1tPLnMnw",
+  authDomain: "sw-mini-project-whatsapp.firebaseapp.com",
+  projectId: "sw-mini-project-whatsapp",
+  storageBucket: "sw-mini-project-whatsapp.appspot.com",
+  messagingSenderId: "604252421931",
+  appId: "1:604252421931:web:d8aac84f0d029272130f6e",
+  measurementId: "G-PY71EDQMBD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app);
